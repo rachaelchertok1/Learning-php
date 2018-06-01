64330bcbc63cb690ec1e3eb7ef24512850ebc837
rachaelchertok1 <33710738+rachaelchertok1@users.noreply.github.com>
2018-06-01 12:58:13 -0400

    Add files via upload
    Learning php through Laracasts, starting with the PHP Practitioner series.
    Homework from Step 10: Functions.
    The task was to create and call a function to check if a person of a certain age is old enough to enter a night club. (Used to reinforce functions and conditionals).

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+function oldEnough($age){
+  if($age >= 21){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'functions.php';
+
+if(oldEnough(15)==true){
+  echo 'Yay! You are old enough to enter the night club!';
+}
+else{
+  echo 'You are NOT old enough to enter the night club.';
+}
